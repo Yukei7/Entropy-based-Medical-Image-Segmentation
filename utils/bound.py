Modify the calculation of region score, add new method to normalize gradient info, correct the calculation of stable metric.

utils/bound.py
@@ -13,8 +13,8 @@ def get_bound(image, bdts_o, bdts_b):
 #         fil = gauss_kernel(kernel_size=1, sigma=1)
         fil = gauss_kernel(kernel_size=1, sigma=1)
         # near edge
-#         bdt = bdts_o[t] + bdts_b[t]
-        bdt = bdts_o[t]
+        bdt = bdts_o[t] + bdts_b[t]
+#         bdt = bdts_o[t]
         edge = np.zeros(shape=bdt.shape)
         # TODO: BDT setting
         edge[np.where(bdt==1)] = 1

utils/homogeneity.py
@@ -41,7 +41,10 @@ def __get_gradvalue_mhue(gradImg, indices, gradcount):
         for j in range(gradImg.shape[1]):
             gradvalue[i, j] = np.sum(gradcount[0:np.argwhere(indices==gradImg[i, j])[0,0] + 1]) / max_gradvalue
     return gradvalue
-            
+
+def normalize_by_var(gradImg):
+    return 1 - np.exp(-((gradImg/255)**2/(np.var(gradImg)/gradImg.size)))
+
     
 def rank_normalize_mhue(gradImg):
     # Rank-normalization

utils/mhue.py
@@ -20,6 +20,7 @@ def get_uncertainty(image, kde=False, off_center=False, info_w=False, stabs=None
     t1 = inten_min + 2
     t2 = inten_max - 2
     uncertainty = []
+    epsilon = 1e-6
 
     for i in range(t1, t2):
         # get index of pixels assigned to object/background
@@ -66,8 +67,11 @@ def get_uncertainty(image, kde=False, off_center=False, info_w=False, stabs=None
             obj_rate = np.array(np.where(img>=i)[0].size / img.size)
             mhue_i[np.where(mhue_i < obj_rate)] = mhue_i[np.where(mhue_i < obj_rate)] / (2*obj_rate)
             mhue_i[np.where(mhue_i >= obj_rate)] = (mhue_i[np.where(mhue_i >= obj_rate)]+1-2*obj_rate) / (2-2*obj_rate)
-
+        
         u = (-mhue_i) * np.log(mhue_i) - (1-mhue_i) * np.log(1-mhue_i)
+        
+        u[np.isnan(u)] = 0
+        
         if info_w:
             # Weighted Information Entropy
             # weight the impurity with context info(distance, stability)
@@ -102,7 +106,7 @@ def get_weights(image,bdts_o,bdts_b,stabs,fil):
     for idx in range(len(stabs)):
         corres_stab = cv.filter2D(stabs[idx],-1,fil)
         corres_stabs.append(corres_stab)
-        weight = (1-np.exp(-corres_stab)+0.1) / 1.1
+        weight = 1-np.exp(-corres_stab)
         weights.append(weight)
     return corres_stabs,weights
 

utils/region.py
@@ -25,16 +25,20 @@ def get_score(image, uncertainty, homogeneity, kernel_size=7, bounding=False):
 def __conv(u,u_pad,scharr_pad,mid_kernel):
     kernel_size = mid_kernel * 2 + 1
     convoluted = np.zeros(u.shape)
+    max_scharr = np.max(scharr_pad)
     for i in range(u.shape[0]):
         for j in range(u.shape[1]):
-            conv_u = u_pad[i:i+2*mid_kernel+1,j:j+2*mid_kernel+1].flatten()
-            conv_scharr = scharr_pad[i:i+2*mid_kernel+1,j:j+2*mid_kernel+1].flatten()
-            # rank
-            u_rank = conv_u.argsort()
-            u_rank = u_rank - np.min(u_rank) + 1
-            # TODO: penalize?
-            kernel_weight = 2*(u_rank/(kernel_size**2))-1
-            convoluted[i,j] = np.sum(kernel_weight*conv_scharr)
+            conv_u = u_pad[i:i+2*mid_kernel+1,j:j+2*mid_kernel+1].copy().flatten()
+            conv_scharr = scharr_pad[i:i+2*mid_kernel+1,j:j+2*mid_kernel+1].copy().flatten()
+#             # rank
+#             u_rank = conv_u.argsort()
+#             u_rank = u_rank - np.min(u_rank) + 1
+#             # TODO: penalize?
+#             kernel_weight = (2*(u_rank/(kernel_size**2))-1)
+
+            kernel_weight = conv_u.copy()
+
+            convoluted[i,j] = np.sum(kernel_weight*conv_scharr) - np.sum(kernel_weight*(max_scharr-conv_scharr))
     return convoluted
 
 

utils/stability.py
@@ -10,51 +10,64 @@ def get_stability(image, bdt, width=3, r_max=2, neighbours_min=3):
     tmin = np.min(image) + 2
     tmax = np.max(image) - 2
     npix = image.size
-    dist_lst = []
+    stab_lst = []
     for t in range(tmin, tmax):
         # current edge
         idx = np.where(bdt[t - tmin].reshape(image.shape) == 1)
         tmp1, tmp2 = idx
-        idx = list(zip(tmp1, tmp2))
-        windows = __windowing(image, bdt, t, width, r_max, neighbours_min, idx)
-        dist_lst.append(np.diff(windows.T,2))
-    output = list(map(lambda x: np.sum(np.abs(x),axis=1).reshape(image.shape), dist_lst))
-    return output
+        idx_lst = list(zip(tmp1, tmp2))
+        windows = __windowing(image, bdt, t, width, r_max, neighbours_min, idx_lst)
+        # Absolute sum of second order differences
+        window_diff = np.abs(np.diff(windows.T,2))
+        # unstab -> stab
+        window_diff = np.exp(-window_diff).reshape(image.shape)
+        
+        stab = np.zeros(image.shape)
+#         print(stab.shape, window_diff.shape, idx.shape)
+        stab[idx] = window_diff[idx]
+        stab_lst.append(stab)
+    return stab_lst
 
 
 @jit(nopython=True)
 def __windowing(image, bdt, t, width, r_max, neighbours_min, idx):
+    # assume width is odd
+    mid_width = width // 2
+    
     tmin = np.min(image) + 2
     tmax = np.max(image) - 2
     npix = image.size
     windows = np.zeros((width,npix))
-    for w in range(1,width+1):
+    window_counter = 0
+    for w in range(-mid_width,mid_width+1):
+        if w == 0:
+            continue
         dist = np.zeros(image.shape)
         # for each point in the current edge, find the neighbours in the next edge.
         for (x, y) in idx:
             if (x == 0) or (y == 0) or (x == image.shape[0] - 1) or (y == image.shape[1] - 1):
                 continue
             r = 1
-            xo = []
-            yo = []
-            while len(xo) < neighbours_min and r < r_max:
+            cord = []
+            while len(cord) < neighbours_min and r < r_max:
                 for m in range(x - r, x + r + 1):
                     for n in range(y - r, y + r + 1):
                         if (m<=0) or (n<=0) or (m>=image.shape[0]-1) or (n>=image.shape[1]-1) or (t-tmin+w>=len(bdt)):
                             continue
-                        if bdt[t - tmin + w][m, n] == 1:
-                            xo.append(m)
-                            yo.append(n)
+                        if bdt[t - tmin + w][m, n] == 1 and [m, n] not in cord:
+                            cord.append([m, n])
                 r += 1
-            if len(xo) < neighbours_min:
+            if len(cord) < neighbours_min:
                 # something large enough
                 dist[x, y] = r_max
                 continue
             # Calculate the mean value of the position
-            xc, yc = np.mean(np.array(xo)), np.mean(np.array(yo))
-#             print(xc, yc)
+            cordt = np.array(cord)
+            xo, yo = cordt[:, 0], cordt[:, 1]
+            xc, yc = np.mean(xo), np.mean(yo)
             dist[x, y] = ((xc-x)**2 + (yc-y)**2)**0.5
-        windows[w-1,] = dist.flatten()
+        windows[window_counter,] = dist.flatten()
+        window_counter += 1
     return windows
 
 